338bbed65b6e7f61a71b166285f54293aa968caa
Nikolas.Young <nikolas_young@foxmail.com>
2026-07-07 22:35:20 +0800

    Add SPI low level api. Need further check.

--- src/pyLibmpsse/spi.py
+++ src/pyLibmpsse/spi.py
@@ -3,7 +3,7 @@ import ctypes
 from dataclasses import dataclass
 from typing import Optional
 
-from .libmpsse_bindings import FT_DEVICE_LIST_INFO_NODE
+from .libmpsse_bindings import FT_DEVICE_LIST_INFO_NODE, ChannelConfig
 from .consts import FT_STATUS
 
 @dataclass(frozen=True)
@@ -30,15 +30,62 @@ class SPIInterface:
             raise Exception(f"Failed to get number of SPI channels. Status: {status}")
         return num_channels
 
-    def SPI_GetChannelInfo(self, index) -> FT_DEVICE_LIST_INFO_NODE:
+    def SPI_GetChannelInfo(self, index, chan_info: ) -> ctypes.c_uint32:
         chan_info = FT_DEVICE_LIST_INFO_NODE()
         status = self.bindings.libmpsse_dll.SPI_GetChannelInfo(index, ctypes.byref(chan_info))
         if status != FT_STATUS.FT_OK.value:
             raise Exception(f"Failed to get channel info for index {index}. Status: {status}")
         return chan_info
     
+    def SPI_OpenChannel(self, index) -> ctypes.c_void_p:
+        handle = ctypes.c_void_p()
+        status = self.bindings.libmpsse_dll.SPI_OpenChannel(index, ctypes.byref(handle))
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to open channel for index {index}. Status: {status}")
+        return handle
     
+    def SPI_InitChannel(self, handle, config: ChannelConfig) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_InitChannel(handle, ctypes.byref(config))
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to initialize channel. Status: {status}")
+        return status
 
+    def SPI_CloseChannel(self, handle) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_CloseChannel(handle)
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to close channel. Status: {status}")
+        return status
+    
+    def SPI_Read(self, handle, buffer, sizeToTransfer, sizeTransferred, transferOptions) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_Read(handle, buffer, sizeToTransfer, ctypes.byref(sizeTransferred), transferOptions)
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to read from channel. Status: {status}")   
+        return status
+    
+    def SPI_Write(self, handle, buffer, sizeToTransfer, sizeTransferred, transferOptions) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_Write(handle, buffer, sizeToTransfer, ctypes.byref(sizeTransferred), transferOptions)
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to write to channel. Status: {status}")
+        return status
+    
+    def SPI_ReadWrite(self, handle, inBuffer, outBuffer, sizeToTransfer, sizeTransferred, transferOptions) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_ReadWrite(handle, inBuffer, outBuffer, sizeToTransfer, ctypes.byref(sizeTransferred), transferOptions)
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to read/write from/to channel. Status: {status}")
+        return status
+    
+    def SPI_IsBusy(self, handle, state) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_IsBusy(handle, ctypes.byref(state))
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to check if channel is busy. Status: {status}")
+        return state
+
+    def SPI_ChangeCS(self, handle, configOptions) -> ctypes.c_uint32:
+        status = self.bindings.libmpsse_dll.SPI_ChangeCS(handle, configOptions)
+        if status != FT_STATUS.FT_OK.value:
+            raise Exception(f"Failed to change chip select. Status: {status}")
+        return status
+    
     """
     Pythonic wrapper methods for SPI functions
     """
